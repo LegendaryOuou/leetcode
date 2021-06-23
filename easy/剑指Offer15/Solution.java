22e97c82952f90c4b658dfbcdb529bdca571cea4
LegendaryOuou <1632795172@qq.com>
2021-06-23 11:14:00 +0800

    完善了这一题的备注

--- easy/剑指Offer15/Solution.java
+++ easy/剑指Offer15/Solution.java
@@ -2,6 +2,10 @@ public class Solution {
 	/*
 	执行用时：1 ms, 在所有 Java 提交中击败了96.60%的用户
 	内存消耗：35.3 MB, 在所有 Java 提交中击败了58.75%的用户
+	
+	怎么它成了每日一题，同样的代码，资源消耗变小了？
+	执行用时：1 ms, 在所有 Java 提交中击败了96.63%的用户
+	内存消耗：35.2 MB, 在所有 Java 提交中击败了82.98%的用户
 	*/
     // you need to treat n as an unsigned value
     public int hammingWeight(int n) {
