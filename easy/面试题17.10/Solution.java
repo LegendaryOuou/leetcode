78c63ec8eb259266046195e2b8325eac60ad3d5d
LegendaryOuou <1632795172@qq.com>
2021-07-09 11:09:18 +0800

    添加了摩尔投票算法实现-07.09

--- easy/面试题17.10/Solution.java
+++ easy/面试题17.10/Solution.java
@@ -15,4 +15,24 @@ class Solution {
         }
         return -1;
     }
+	/*
+	摩尔投票
+	知道要用摩尔投票，但是钻牛角尖的一直认为O(n)只需要一次遍历，没考虑到最后还要统计一下结果是不是正确的
+	
+	执行用时：1 ms, 在所有 Java 提交中击败了100.00%的用户
+	内存消耗：44.3 MB, 在所有 Java 提交中击败了18.79%的用户
+	*/
+	public int majorityElement(int[] nums) {
+        int sum=0;
+        int res=0;
+        for(int num:nums){
+            if(sum==0) res=num;
+            sum+=(res==num?1:-1);
+        }
+        sum=0;
+        for(int num:nums){
+            if(num==res) sum++;
+        }
+        return sum*2>nums.length?res:-1;
+    }
 }
